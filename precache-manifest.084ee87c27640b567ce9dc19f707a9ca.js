3a0f675c35085ddd8af47019841ad51f074c3850
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-16 15:24:36 +0200

    Updates

---
+++ precache-manifest.084ee87c27640b567ce9dc19f707a9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3ad155ed1ba055a79a02a90c893c1f",
+    "url": "/React_Router/index.html"
+  },
+  {
+    "revision": "d1179ad5ae53e84f1f40",
+    "url": "/React_Router/static/css/main.9ec23b9b.chunk.css"
+  },
+  {
+    "revision": "e43b9ffb9cd5fc50739c",
+    "url": "/React_Router/static/js/2.16cde3c2.chunk.js"
+  },
+  {
+    "revision": "d1179ad5ae53e84f1f40",
+    "url": "/React_Router/static/js/main.85ddf2f1.chunk.js"
+  },
+  {
+    "revision": "ab0c7cdeb1cbe5386e08",
+    "url": "/React_Router/static/js/runtime~main.ff40dc53.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/React_Router/static/media/pokeball.f10bf078.png"
+  }
+]);
