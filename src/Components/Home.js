60d2f441efec472969f15421398836f09d9aac71
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-18 16:42:45 +0200

    connecting components to the root reducer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -28,10 +28,10 @@ class Home extends Component {
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
-                        <img src={Pokeball} alt=""/>
+                        <img src={Pokeball} alt="" />
                         <div className="card-content">
-                            <Link to={ '/' + post.id } >
-                            <span className="card-title red-text">{post.title}</span>
+                            <Link to={'/' + post.id} >
+                                <span className="card-title red-text">{post.title}</span>
                             </Link>
                             <p>{post.body}</p>
                         </div>
